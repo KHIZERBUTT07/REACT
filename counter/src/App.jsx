9ff5eec376c14f4d93997d42594bf69da9026f43
KHIZERBUTT07 <153557362+KHIZERBUTT07@users.noreply.github.com>
2024-07-31 10:58:35 +0500

    StartedReact

---
+++ counter/src/App.jsx
@@ -0,0 +1,18 @@
+import Button from "./components/Button"
+function App() {
+  
+
+  return (
+    
+     <div >
+     <h1> Hello React!</h1>
+     <Button text="1st Btn" />
+     <Button text="2nd Btn" />
+     <Button text="3rd Btn" />
+     <Button text="4th Btn" />
+    
+     </div>
+  )
+} 
+
+export default App
