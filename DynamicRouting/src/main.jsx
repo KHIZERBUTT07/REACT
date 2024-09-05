be0d64c37bd744f090e533b8001e0eb72cce3ec3
KHIZERBUTT07 <153557362+KHIZERBUTT07@users.noreply.github.com>
2024-09-06 02:34:29 +0500

    dynamicRouting

--- DynamicRouting/src/main.jsx
+++ DynamicRouting/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { NextUIProvider } from "@nextui-org/react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
+  </React.StrictMode>
+);
