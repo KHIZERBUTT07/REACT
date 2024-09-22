4d596bdbf34ff71d137ae275a34537601e8e0d0f
KHIZERBUTT07 <153557362+KHIZERBUTT07@users.noreply.github.com>
2024-09-22 20:54:18 +0500

    createdUnplashGallery

--- SplashGallery/src/App.jsx
+++ SplashGallery/src/App.jsx
@@ -4,11 +4,16 @@ import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import Header from './components/Header';
 import { Masonry } from '@mui/lab';
+import { Modal } from '@mui/material';
+import { Avatar } from 'antd';
 
 function App() {
 
 const  [images , setImages] = useState([])
 const  [loading , setLoading] = useState(true)
+const  [search , setSearch] = useState('')
+const  [showModal, setShowModal] = useState(false)
+const  [chosen, setChosen] = useState(null)
 
 useEffect(()=>{
   getImagesFromUnsplash();
@@ -24,8 +29,81 @@ const  getImagesFromUnsplash = ()=>{
 .catch((err) => setLoading(false))
 };
 
+
+
+const  SearchPhotosFromUnsplash = ()=>{
+
+  if(search){
+    fetch(`https://api.unsplash.com/search/photos?page=1&query=${search}&client_id=oTO49XdAaqBeNW1n3tO0fOX6Qkhi_tC1UK4NvmQql-Q&per_page=30`)
+    .then((res)=>res.json())
+    .then((res) => {
+      setImages(res.results)
+      console.log("search res",res)
+    setLoading(false);
+  })
+  .catch((err) => setLoading(false))
+  }
+};
+
+
+
   return (
     <div>
+
+    <Modal 
+
+    open={showModal}
+    onClose={()=>setShowModal(false)}
+    sx={{
+      display:'flex',
+      justifyContent:'center',
+      alignItems:'center',
+     
+      
+    }}>
+      <Box 
+       sx={{
+      display:'flex',
+      justifyContent:'center',
+      width:'80%',
+      overflow:'hidden',
+    }} 
+    >
+      <img 
+      
+      width={"50%"}
+      className='contain'
+      src={chosen?.urls?.regular}
+      style={{
+        maxHeight:550
+      }}
+      />
+
+      <Box display={"block"} width={"50%"} padding={2} bgcolor={'white'}>
+        <Typography fontWeight={"bold"} variant="h4">
+         {chosen.alt_description}
+        </Typography>
+
+        <Box display={"flex"} sx={{
+          marginTop:2,
+          marginBottom:2,
+        }}>
+          <Avatar sizes="medium"
+          src={chosen?.sponsorship?.sponsor?.profile_image?.medium  || chosen?.user?.profile_image?.medium  } />
+          <Box marginLeft={1}>
+            <Typography>  {chosen?.sponsorship?.sponsor?.name || chosen?.user?.name} </Typography>
+            <Typography> Total Photos :  {chosen?.sponsorship?.sponsor?.total_photos || chosen?.user?.total_photos} </Typography>
+          </Box>
+        </Box>
+
+        <Typography fontWeight={"medium"} variant="h6">
+          Likes : {chosen.likes}
+        </Typography>
+        
+      </Box>
+      </Box>
+     
+    </Modal>
       <Header/>
     <div className='container mx-auto'>
       
@@ -75,11 +153,15 @@ const  getImagesFromUnsplash = ()=>{
           fullWidth
           variant="outlined"
           color="secondary"
+          value={search}
+          onChange={(e)=> setSearch(e.target.value)}
         />
         <Button
           endIcon={<SearchOutlinedIcon />}
           variant="outlined"
+          disabled = {search === '' || search.length < 2}
           color="secondary"
+          onClick={SearchPhotosFromUnsplash}
       
           sx={{
             
@@ -108,9 +190,15 @@ const  getImagesFromUnsplash = ()=>{
       columns={{sm:1 , md:2 , lg:3 , xl:4}}
       spacing={2}
       >
-        {
+        { 
           images.map((item)=>(
-          <img key={item.id} src={item.urls.regular} />
+          <img 
+          onClick={() => {
+            setShowModal(true);
+            setChosen(item)
+          
+          }}
+          key={item.id} src={item.urls.regular} />
         )) }
       </Masonry>
       </div>
